5d51d2c2516e8b4991d46667b935ea730c3e9295
mveratam <mveratam@emeal.nttdata.com>
2023-12-20 15:32:05 -0500

    ejercicios

--- src/main/java/mains/Ejercicios.java
+++ src/main/java/mains/Ejercicios.java
@@ -4,16 +4,47 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
-public class FilterLambda {
+public class Ejercicios {
 
 	public static void main(String[] args) {
 		String str = "aaabbcc";// output a3b2c2
-
-		System.out.println(countCharacterFunctional(str));
+		int entrada = 9999992;
+//		System.out.println(countCharacterFunctional(str));
+		System.out.println(recursividadSumaDigitos(entrada));
 
 	}
 
+	/* 
+	 * crear un metodo que dado un numero entero, suma repetidamente 
+	 * todos sus digitos hasta que solo tenga un digito y devuelvelo
+	 * ejemplo entrada 38
+	 * 3+8 -> 11 1+1 -> 2
+	 * ejemplo salida 2
+	 * 
+	 */
+	
+	private static int recursividadSumaDigitos(int entrada) {
+		if(String.valueOf(entrada).length() == 1) {
+			return entrada;
+		}
+		else {
+			System.out.println("entrada : " + String.valueOf(entrada));
+			Integer result = Stream.of(String.valueOf(entrada)
+			.split(""))
+			.map(Integer::parseInt)
+			.reduce(0, (x,y) -> x+y);
+			return recursividadSumaDigitos(result);
+		}
+	}
+	
+	/* 
+	 * crear un metodo que cuente la cantidad de caracteres en un string
+	 * ejemplo entrada aaabbbccc
+	 * ejemplo salida a3b3c3
+	 * 
+	 */
 	private static String countCharacter(String str) {
 		Map<Character, Integer> map = new HashMap<>();
 		StringBuilder stringBuilder = new StringBuilder();
